53566f812a6a5d945d9dca5f0a2ba8e2510ba3d3
Alexis Cano <156945503+aic918@users.noreply.github.com>
2026-03-07 02:26:23 +0000

    word_frequency working

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,3 +1,11 @@
+mod word_frequency;
+
+use word_frequency::most_frequent_word;
+
 fn main() {
-    println!("Hello, world!");
+    let text = "the quick brown fox jumps over the lazy dog the quick brown fox";
+
+    let (word, count) = most_frequent_word(text);
+
+    println!("Most frequent word: \"{}\" ({} times)", word, count);
 }
---
+++ my_project/target/debug/deps/my_project-f87ffb0199ca160e.d
@@ -0,0 +1,6 @@
+/workspaces/SpringSystems-01/my_project/target/debug/deps/my_project-f87ffb0199ca160e.d: src/main.rs src/word_frequency.rs
+
+/workspaces/SpringSystems-01/my_project/target/debug/deps/my_project-f87ffb0199ca160e: src/main.rs src/word_frequency.rs
+
+src/main.rs:
+src/word_frequency.rs:
